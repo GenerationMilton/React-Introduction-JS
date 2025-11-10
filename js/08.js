3e24161910321ad411456f7b039664d31d84d078
Milton Munoz <livemilton@gmail.com>
2025-11-09 20:33:20 -0500

    introduction js :sparkles: Template strings and concatenation

---
+++ js/08.js
@@ -0,0 +1,11 @@
+// Template Strings and concatenation
+
+const product = "Tablet de 12 Pulgadas";
+const price = 400;
+const brand = "Orange";
+
+console.log('El Producto es: ' + product);
+console.log(`El producto es: ${product}`);
+
+console.log(product + " $" + price + " Dolares, marca: " + brand);
+console.log(`${product} $${price} Dolares, narca: ${brand}`);
