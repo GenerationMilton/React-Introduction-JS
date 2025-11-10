5914764d8b60f351c57cc553d0d69130b315b220
Milton Munoz <livemilton@gmail.com>
2025-11-09 21:50:29 -0500

    introduction js :sparkles: Iterate an arrays with for, map, for of

--- js/12.js
+++ js/12.js
@@ -1,7 +1,7 @@
 // iterate with For in arrays
 const technologies = ['HTML','CSS','JavaScript','React.js','Node.js', 'Nest.js'];
 
-console.log("access manually")
+console.log("**access manually**")
 console.log(technologies[0]);
 console.log(technologies[1]);
 console.log(technologies[2]);
@@ -9,8 +9,31 @@ console.log(technologies[3]);
 console.log(technologies[4]);
 console.log(technologies[5]);
 
-//example
-console.log("access with for");
+//example 1
+console.log("**access with for**");
 for(let i = 0; i<technologies.length; i++){
     console.log(technologies[i]);
 }
+
+//Foreach example 2
+console.log("**access forEach**")
+technologies.forEach(function(tech){
+console.log(tech)
+});
+ 
+// map example 3 -- generates an new array
+console.log("**access map**")
+const arrayMap = technologies.map(function(tech){
+    return tech
+});
+
+console.log(arrayMap);
+
+// for ...of executes an block code
+console.log("**access for of**")
+for(let tech of technologies){
+    console.log(tech);
+}
+
+
+
