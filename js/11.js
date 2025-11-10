f8427b1474fffe06776e16db7efca90581597fb5
Milton Munoz <livemilton@gmail.com>
2025-11-09 21:22:20 -0500

    introduction js :sparkles: Destructuring arrays

---
+++ js/11.js
@@ -0,0 +1,11 @@
+// Destructuring arrays
+const technologies = ['HTML','CSS','JavaScript','React.js','Node.js'];
+
+// First sintax
+const react = technologies[3];
+console.log(react);
+
+// Destructuring arrays
+const [, , , reactjs] = technologies;
+
+console.log(reactjs);
