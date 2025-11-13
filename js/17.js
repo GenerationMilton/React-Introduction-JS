0992c2d18d18e26c06dd7faba57518549b575789
Milton Munoz <livemilton@gmail.com>
2025-11-12 22:36:25 -0500

    introduction js :sparkles: array methods part2

--- js/17.js
+++ js/17.js
@@ -21,3 +21,28 @@ console.log(newArray);
 console.log(technologies2);
 console.log(result);
 console.log(result2);
+
+//some - return someone if almost least is true 
+console.log("result Some");
+const resultSome = numbers.some(number => number >15)
+console.log(resultSome);
+
+//Find - return the first element that fulfills the condition
+console.log("result Find");
+const resultFind = numbers.find( number => number > 15);
+console.log(resultFind);
+
+// Every - Return true or false if all fulfill the condition
+console.log("result Every");
+const resultEvery = numbers.every(number => number > 15);
+console.log(resultEvery);
+
+// Reduce - returns a cumulative total
+const resultReduce = numbers.reduce((total, number) =>{
+    console.log(total)
+    console.log(number)
+
+    return total + number
+},0)
+
+console.log(resultReduce);
