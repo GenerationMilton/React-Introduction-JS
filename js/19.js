9b290d318a85df5ba86659194e7dc4b442e8f2ad
Milton Munoz <livemilton@gmail.com>
2025-11-12 22:53:33 -0500

    introduction js :sparkles: strict comparator

---
+++ js/19.js
@@ -0,0 +1,15 @@
+
+const number1 = 20;
+const number2 = "20";
+
+
+/*
+    == (non-strict comparator)
+    === (Strict comparator)
+
+*/
+if(number1 === number2){
+    console.log('Si, son iguales');
+} else {
+    console.log('No, no son iguales');
+}
