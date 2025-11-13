aaabd6eeb6d51c900f7d93bbecf57a0b99bc0e13
Milton Munoz <livemilton@gmail.com>
2025-11-12 23:10:35 -0500

    introduction js :sparkles: Logical Or and Logical And

---
+++ js/20.js
@@ -0,0 +1,15 @@
+const balance = 1000;
+const pay = 500;
+const card = false;
+
+/**
+ *  Logical Or Logical And
+ *  ||
+ *  &&
+ * 
+ */
+if(balance > pay || card){
+    console.log('Puedes Pagar')
+} else {
+    console.log('No no puedes pagar')
+};
