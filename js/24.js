5aec4d4e441659c661592aaa3f333e80d753a354
Milton Munoz <livemilton@gmail.com>
2025-11-13 22:29:42 -0500

    introduction js :sparkles: ECMAScript Modules parte3

--- js/24.js
+++ js/24.js
@@ -1,5 +1,12 @@
-import { sumar as funcionSumar, restar } from './funciones.js'
+
+import multiplicar, { sumar as funcionSumar, restar, division } from './funciones.js'
 
 const resultado = funcionSumar(40, 10)
+const resultatoMultiplicar = multiplicar(10, 10);
+const resultadoDividir = division(20, 10);
+const resultadoRestar = restar(10, 5);
 
 console.log(resultado);
+console.log(resultatoMultiplicar);
+console.log(resultadoDividir);
+console.log(resultadoRestar);
--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,4 @@
-function sumar(n1, n2){
+const sumar = (n1, n2) =>{
     return n1 + n2
 }
 
@@ -6,7 +6,18 @@ function restar(n1, n2){
     return n1 - n2
 }
 
+const division = (n1, n2) => {
+    return n1 / n2;
+}
+
+export default function multiplicar (n1, n2){
+    return n1 * n2
+}
+
+
+
 export {
     sumar,
-    restar
+    restar,
+    division
 }
