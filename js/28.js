1037d7dd20fb9903e5ea9de923429235115c34fa
Milton Munoz <livemilton@gmail.com>
2025-11-15 12:58:14 -0500

    introduction js :sparkles: DOM select elements

---
+++ js/28.js
@@ -0,0 +1,13 @@
+// Selectors
+
+//query selector return one element
+const heading = document.querySelector('.heading')
+console.log(heading);
+console.log(heading.tagName)
+console.log(heading.textContent)
+console.log(heading.classList)
+console.log(heading.id)
+
+//query selector all return one or more elements
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces);
