0e149168c03d9701a57a18a198c811ba7ae110bc
Milton Munoz <livemilton@gmail.com>
2025-11-15 15:06:35 -0500

    introduction js :sparkles: DOM validate Forms and show alerts

--- js/32.js
+++ js/32.js
@@ -7,14 +7,33 @@ formulario.addEventListener('submit', event => {
     const nombre = document.querySelector('#nombre').value
     const password = document.querySelector('#password').value
 
+    //prevent new alerts
+    const alertaPrevia = document.querySelector('.alerta')
+    if(alertaPrevia){
+        alertaPrevia.remove()
+    }
 
+    //generate alert
+    const alerta = document.createElement('DIV')
+    // alerta.textContent = 'Contenido de Alerta'
+    // add clases
+    alerta.classList.add('alerta','text-white','uppercase','text-sm','text-center','p-2','font-black')
+   
+
+    // make conditional to show the alert
     if (nombre === '' || password === '') {
-        console.log('Todos los campos son obligatorios...')
+        alerta.textContent = 'Todos los campos son obligatorios...';
+        alerta.classList.add('bg-red-500');
     }else {
-        console.log(nombre)
-        console.log(password)
-        console.log('Enviando Formulario...')
+        alerta.textContent = 'Todo bien, iniciando sesión...'
+        alerta.classList.add('bg-green-500')
     }
    
+    //show alert in html with childs of forms
+    formulario.appendChild(alerta)
+
+    setTimeout(() => {
+        alerta.remove()
+    }, 3000);
 })
 
